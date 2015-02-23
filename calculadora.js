45b5066b23e863409b02a5ca33ffb91859214770
Jona <jona@jona-VirtualBox.(none)>
2015-02-23 23:50:53 +0100

    Vuelvo a subir ejercicio por error :disappointed:

--- calculadora.js
+++ calculadora.js
@@ -1,60 +1,36 @@
+var sumando="";
+var estado=false;
+function unos() {
+	var dis = document.getElementById("di");
+	if(dis.innerHTML=="Display" || estado==true){
+		dis.innerHTML = "1";
+		estado=false;
+ 	}else
+ 	dis.innerHTML = "1"+dis.innerHTML;
 
-var teclas = document.querySelectorAll('#calculadora span');
-var ops = ['+', '-', 'x', '/'];
-var decimalAdded = false;
+}
 
-for(var i = 0; i < teclas.length; i++) {
-	teclas[i].onclick = function(e) {
-		
-		var entrada = document.querySelector('.screen');
-		var entradaVal = entrada.innerHTML;
-		var btnVal = this.innerHTML;
+function ceros() {
+	var dis = document.getElementById("di");
+	if(dis.innerHTML=="Display"|| estado==true){
+	dis.innerHTML = "0";
+	estado=false;
+	}else
+	dis.innerHTML = "0"+dis.innerHTML;
+}
 
-		if(btnVal == 'C') {
-			entrada.innerHTML = '';
-			decimalAdded = false;
-		}
+function suma() {
+	sumando = parseInt(document.getElementById("di").innerHTML,2);
+	estado=true;
+}
 
-		else if(btnVal == '=') {
-			var equacion = entradaVal;
-			var ultimo = equacion[equacion.length - 1];
+function resultado() {
+	oper = parseInt(document.getElementById("di").innerHTML,2);
+	document.getElementById("di").innerHTML = oper + sumando;
+}
 
-			equacion = equacion.replace(/x/g, '*').replace(/÷/g, '/');
-
-			if(ops.indexOf(ultimo) > -1 || ultimo == '.')
-				equacion = equacion.replace(/.$/, '');
-			
-			if(equacion)
-				entrada.innerHTML = eval(equacion);
-				
-			decimalAdded = false;
-		}
-
-		else if(ops.indexOf(btnVal) > -1) {
-			var ultimo = entradaVal[entradaVal.length - 1];
-
-			if(entradaVal != '' && ops.indexOf(ultimo) == -1) 
-				entrada.innerHTML += btnVal;
-
-			else if(entradaVal == '' && btnVal == '-') 
-				entrada.innerHTML += btnVal;
-
-			if(ops.indexOf(ultimo) > -1 && entradaVal.length > 1) {
-				entrada.innerHTML = entradaVal.replace(/.$/, btnVal);
-			}
-			
-			decimalAdded =false;
-		}
-		else if(btnVal == '.') {
-			if(!decimalAdded) {
-				entrada.innerHTML += btnVal;
-				decimalAdded = true;
-			}
-		}
-
-		else {
-			entrada.innerHTML += btnVal;
-		}
-		e.preventDefault();
-	} 
+function nuevo() {
+	sumando="";
+	estado=true;
+	document.getElementById("di").innerHTML="0";
 }
